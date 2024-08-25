03fae834bba10cebd56c7e353fb7c2fcc4d4577f
Luis Alberto Perez <luisalbertoperez@gmail.com>
2024-08-25 18:29:10 -0400

    avance

--- src/app/products/data-access/products.service.ts
+++ src/app/products/data-access/products.service.ts
@@ -1,11 +1,9 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BaseHttpService } from '../../shared/data-access/base-http.service';
 
 @Injectable()
-export class ProductsService {
-  constructor(private http: HttpClient) {}
-
+export class ProductsService extends BaseHttpService {
   getProducts() {
-    return this.http.get('https://fakestoreapi.com/products/');
+    return this.http.get(`${this.apiUrl}/products`);
   }
 }
