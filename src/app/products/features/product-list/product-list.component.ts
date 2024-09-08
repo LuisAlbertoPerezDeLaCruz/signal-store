2d40a553c67209e2383fc7f415a5546d397f2b11
Luis Alberto Perez <luisalbertoperez@gmail.com>
2024-09-08 17:29:02 -0400

    avance

--- src/app/products/features/product-list/product-list.component.ts
+++ src/app/products/features/product-list/product-list.component.ts
@@ -1,10 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { ProductsStateService } from '../../data-access/products-state.service';
+import { ProductCardComponent } from '../../ui/product-card/product-card.component';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
   templateUrl: './product-list.component.html',
+  imports: [ProductCardComponent],
   providers: [ProductsStateService],
 })
 export default class ProductListComponent {
