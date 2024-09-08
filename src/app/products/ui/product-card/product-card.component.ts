4e9eb9bc723239972ce2700c942fea7b75faf4fc
Luis Alberto Perez <luisalbertoperez@gmail.com>
2024-09-08 18:28:53 -0400

    avance

--- src/app/products/ui/product-card/product-card.component.ts
+++ src/app/products/ui/product-card/product-card.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { Product } from '../../../shared/interfaces/product.interface';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
   imports: [],
   templateUrl: './product-card.component.html',
-  styles: ``
+  styles: ``,
 })
 export class ProductCardComponent {
-
+  product = input.required<Product>();
 }
