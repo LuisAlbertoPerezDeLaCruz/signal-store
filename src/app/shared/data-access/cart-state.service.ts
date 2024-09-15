6de144934bf8f23328239ed3dc78402057102a45
Luis Alberto Perez <luisalbertoperez@gmail.com>
2024-09-15 07:13:12 -0400

    ajuste

--- src/app/shared/data-access/cart-state.service.ts
+++ src/app/shared/data-access/cart-state.service.ts
@@ -2,36 +2,30 @@ import { inject, Injectable } from '@angular/core';
 import { signalSlice } from 'ngxtension/signal-slice';
 import { StorageService } from './storage.service';
 import { map } from 'rxjs';
-import {  ProductItemCart } from '../interfaces/product.interface';
+import { ProductItemCart } from '../interfaces/product.interface';
 
-@Injectable({
-  providedIn: 'root',
-})
-  
 interface State {
   products: ProductItemCart[];
   loaded: boolean;
 }
-  
-export class CartStateService {
-  
 
+@Injectable({
+  providedIn: 'root',
+})
+export class CartStateService {
   private storageService = inject(StorageService);
 
   private initialState: State = {
-    products:[],
+    products: [],
     loaded: false,
   };
 
-  loadProducts$ = this.storageService.loadProducts()
-    .pipe(map((products) =>({products,loaded:true})));
+  loadProducts$ = this.storageService
+    .loadProducts()
+    .pipe(map((products) => ({ products, loaded: true })));
 
   public state = signalSlice({
     initialState: this.initialState,
-    sources: [
-      this.loadProducts$,
-    ]
+    sources: [this.loadProducts$],
   });
 }
-
-
